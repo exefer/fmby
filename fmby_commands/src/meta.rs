0ec69f1d98075a728658b2aab8dc7c82c6130470
exefer <jessieodeh08@gmail.com>
2025-09-27 20:52:24 +0200

    fix: make register command require being an app dev

--- fmby_commands/src/meta.rs
+++ fmby_commands/src/meta.rs
@@ -46,7 +46,7 @@ pub async fn uptime(ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
-#[poise::command(prefix_command, hide_in_help, required_permissions = "ADMINISTRATOR")]
+#[poise::command(prefix_command, owners_only, hide_in_help)]
 async fn register(ctx: Context<'_>) -> Result<(), Error> {
     poise::builtins::register_application_commands_buttons(ctx).await?;
 
