855c553fa12f7b2678490739aee51ac1eca9c495
exefer <jessieodeh08@gmail.com>
2025-10-21 19:02:41 +0200

    refactor: replace qualified path with imported item

--- fmby_core/src/structs.rs
+++ fmby_core/src/structs.rs
@@ -1,11 +1,12 @@
-use crate::{drama::DramaConfig, error::Error, rss::RssConfig};
+use crate::{database::FmbyDatabase, drama::DramaConfig, error::Error, rss::RssConfig};
+
 pub type Context<'a> = poise::Context<'a, Data, Error>;
 pub type Command = poise::Command<Data, Error>;
 
 pub struct Data {
     pub time_started: std::time::Instant,
     pub has_started: std::sync::atomic::AtomicBool,
-    pub database: crate::database::FmbyDatabase,
+    pub database: FmbyDatabase,
     pub rss_config: RssConfig,
     pub drama_config: DramaConfig,
 }
