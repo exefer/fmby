bb79e1f591fe24e7f084353428dbdad0a87c1a68
exefer <jessieodeh08@gmail.com>
2026-07-09 20:08:48 +0200

    fix: handle shutdown signals for container-friendly exits

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,35 @@ use poise::serenity_prelude::{self as serenity, GatewayIntents};
 use sea_orm::{ConnectOptions, Database};
 use tracing_subscriber::EnvFilter;
 
+#[cfg(unix)]
+pub async fn shutdown_signal() {
+    use tokio::signal::unix::{SignalKind, signal};
+
+    let (mut s1, mut s2, mut s3) = (
+        signal(SignalKind::hangup()).unwrap(),
+        signal(SignalKind::interrupt()).unwrap(),
+        signal(SignalKind::terminate()).unwrap(),
+    );
+
+    tokio::select! {
+        _ = s1.recv() => {},
+        _ = s2.recv() => {},
+        _ = s3.recv() => {},
+    }
+}
+
+#[cfg(windows)]
+pub async fn shutdown_signal() {
+    use tokio::signal::windows;
+
+    let (mut s1, mut s2) = (windows::ctrl_c().unwrap(), windows::ctrl_break().unwrap());
+
+    tokio::select! {
+        _ = s1.recv() => {},
+        _ = s2.recv() => {},
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let _ = dotenvy::dotenv();
@@ -51,7 +80,7 @@ async fn main() {
         .await
         .expect("Failed to connect to database!");
 
-    let client = serenity::Client::builder(token, intents)
+    let mut client = serenity::Client::builder(token, intents)
         .framework(Box::new(framework))
         .event_handler(Arc::new(fmby_events::Handler))
         .data(Arc::new(fmby_core::structs::Data {
@@ -61,7 +90,15 @@ async fn main() {
             rss_config: fmby_core::rss::RssConfig::default(),
             drama_config: fmby_core::drama::DramaConfig::from_config(),
         }))
-        .await;
+        .await
+        .expect("failed to create client");
 
-    client.unwrap().start().await.unwrap();
+    let shutdown = client.shard_manager.get_shutdown_trigger();
+
+    tokio::spawn(async {
+        shutdown_signal().await;
+        shutdown();
+    });
+
+    client.start().await.unwrap();
 }
